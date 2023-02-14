07213552e7bae68bb71a0f21ae61fa1a4a301cf5
milessteamaccount <ronaldlamdev@gmail.com>
2023-02-13 19:36:05 -0600

    installed morgan to use as middleware

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.status(200).json({
     status: 'success',
-  })
-})
-
-app.get('/siamesecat', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      name: 'Siamese Twins'
-    }
   })
 })
 
