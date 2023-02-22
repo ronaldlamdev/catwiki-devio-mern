0a25f1daa9752dcf7c00545ac28f68c16e65aa2c
milessteamaccount <ronaldlamdev@gmail.com>
2023-02-21 23:30:19 -0600

    created search bar

--- frontend/src/assets/sections/Search.tsx
+++ frontend/src/assets/sections/Search.tsx
@@ -13,11 +13,14 @@ const Search = () => {
           <source srcSet={HeroCatMd} media='(768px <= width < 1023px)'/>
           <img src={HeroCatSm} className='w-full rounded-t-3xl' alt='Cat'/>
         </picture>
-        <div className='absolute top-0'>
+        <div className='absolute top-3 left-7'>
           <img src={Logo} className='invert contrast-[150%]' alt='logo'/>
           <h1 className='text-white'>Get to know more about your breed</h1>
-          <div>
-            
+          <div className='flex'>
+            <input type='text' placeholder='Search' className='pl-4 rounded-l-3xl'/>
+            <div className='bg-white flex justify-center rounded-r-3xl border-l-slate-200 border-l w-10'>
+              <img src={SearchIcon} alt='search icon'/>
+            </div>
           </div>
         </div>
       </div>
