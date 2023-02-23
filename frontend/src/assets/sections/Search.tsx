f6888a628675cc91d78bd10fd6e652e8ed6baa8d
milessteamaccount <ronaldlamdev@gmail.com>
2023-02-22 23:08:16 -0600

    added some media queires to the text div in search div

--- frontend/src/assets/sections/Search.tsx
+++ frontend/src/assets/sections/Search.tsx
@@ -7,22 +7,31 @@ import HeroCatLg from '../images/HeroImagelg.png';
 const Search = () => {
   return (
     <div className='px-[1.125rem] mt-8'>
+
+      {/* Cat image and search */}
       <div className='w-full relative'>
         <picture>
-          <source srcSet={HeroCatLg} media='(width > 1024px)'/>
-          <source srcSet={HeroCatMd} media='(768px <= width < 1023px)'/>
+          <source srcSet={HeroCatLg} media='(min-width: 1024px)'/>
+          <source srcSet={HeroCatMd} media='(min-width: 768px)'/>
           <img src={HeroCatSm} className='w-full rounded-t-3xl' alt='Cat'/>
         </picture>
-        <div className='absolute top-3 left-7'>
-          <img src={Logo} className='invert contrast-[150%]' alt='logo'/>
-          <h1 className='text-white'>Get to know more about your breed</h1>
+        <div className='flex flex-col justify-center h-full items-start absolute top-3 left-7 w-[135px] min-[465px]:w-[160px] min-[550px]:w-[200px]'>
+          <div>
+            <img src={Logo} className='invert contrast-[150%]' alt='logo'/>
+            <h1 className='text-white text-sm min-[465px]:text-base'>Get to know more about your breed</h1>
+          </div>
           <div className='flex'>
-            <input type='text' placeholder='Search' className='pl-4 rounded-l-3xl'/>
+            <input type='text' placeholder='Search' className='pl-4 text-sm rounded-l-3xl w-[90px] min-[465px]:w-[120px] min-[550px]:w-[150px] min-[465px]:text-base'/>
             <div className='bg-white flex justify-center rounded-r-3xl border-l-slate-200 border-l w-10'>
               <img src={SearchIcon} alt='search icon'/>
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Discover breeds */}
+      <div>
+
       </div>
     </div>
   )
