71c6f38374d8ebca68c2263320b9e959d0a08dcd
milessteamaccount <ronaldlamdev@gmail.com>
2023-02-23 21:30:08 -0600

    finished styling the search section in search component acrss all screen sizes

--- frontend/src/assets/sections/Search.tsx
+++ frontend/src/assets/sections/Search.tsx
@@ -15,13 +15,13 @@ const Search = () => {
           <source srcSet={HeroCatMd} media='(min-width: 660px)'/>
           <img src={HeroCatSm} className='rounded-t-3xl' alt='Cat'/>
         </picture>
-        <div className='flex justify-center flex-col gap-3 h-full absolute top-0 left-7 max-w-[371px] w-[135px] min-[580px]:w-[220px] min-[768px]:w-[300px]'>
+        <div className='flex justify-center flex-col gap-3 h-full absolute top-0 left-7 max-w-[371px] w-[135px] min-[580px]:w-[220px] min-[768px]:w-[300px] min-[900px]:w-[400px]'>
           <div>
             <img src={Logo} className='invert contrast-[150%] min-[580px]:w-full max-w-[310px]' alt='logo'/>
-            <h1 className='text-white text-sm min-[580px]:text-base mt-4 max-w-[371px]'>Get to know more about your breed</h1>
+            <h1 className='text-white text-sm min-[580px]:text-base mt-4'>Get to know more about your breed</h1>
           </div>
           <div className='flex'>
-            <input type='text' placeholder='Search' className='pl-4 text-sm min-[580px]:text-base rounded-l-3xl max-w-[394px] w-[90px] min-[580px]:w-[180px] min-[768px]:w-[240px]'/>
+            <input type='text' placeholder='Search' className='pl-4 text-sm min-[580px]:text-base rounded-l-3xl max-w-[394px] w-[90px] min-[580px]:w-[180px] min-[768px]:w-[270px] min-[900px]:w-[360px] lg:w-[394px]'/>
             <div className='bg-white flex justify-center rounded-r-3xl border-l-slate-200 border-l w-10'>
               <img src={SearchIcon} alt='search icon' className='cursor-pointer'/>
             </div>
