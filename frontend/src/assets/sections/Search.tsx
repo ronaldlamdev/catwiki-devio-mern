63720d536ba9a2d45401d36572e1dfe93d0ddcdd
milessteamaccount <ronaldlamdev@gmail.com>
2023-02-24 23:20:10 -0600

    stylized the discover breeds section for mobile with placeholder images

--- frontend/src/assets/sections/Search.tsx
+++ frontend/src/assets/sections/Search.tsx
@@ -4,6 +4,10 @@ import Logo from '../images/CatwikiLogo.svg';
 import HeroCatSm from '../images/HeroImagesm.png';
 import HeroCatMd from '../images/HeroImagemd.png';
 import HeroCatLg from '../images/HeroImagelg.png';
+import Placeholder1 from '../images/Placeholder1.jpg';
+import Placeholder2 from '../images/Placeholder2.jpg';
+import Placeholder3 from '../images/Placeholder3.jpg';
+import Placeholder4 from '../images/Placeholder4.jpg';
 
 const Search = () => {
   return (
@@ -31,7 +35,9 @@ const Search = () => {
       </div>
 
       {/* Discover breeds */}
-      <div className='bg-[#E3E1DC] rounded-b-3xl px-7 pt-3'>
+      <div className='bg-[#E3E1DC] rounded-b-3xl px-7 pt-3 pb-14 flex flex-col gap-6'>
+
+        {/* Text */}
         <div className='flex flex-col gap-4'>
           <div>
             <h2 className='text-[#291507] font-medium text-sm'>Most Searched Breeds</h2>
@@ -40,6 +46,38 @@ const Search = () => {
           <div className='flex justify-between items-center'>
             <h1 className='text-[#291507] font-bold text-lg'>66+ Breeds For you to discover</h1>
             <span className='hidden min-[580px]:flex items-center text-[#29150799] font-bold text-lg gap-2'>SEE MORE <BsArrowRight /></span>
+          </div>
+        </div>
+
+        {/* Breeds images */}
+        <div className='grid grid-cols-2 gap-3'>
+          {/* First grid item */}
+          <div className='flex flex-col gap-3'>
+            <div className='w-[135px] h-[135px]'>
+              <img className='w-full h-full rounded-xl' src={Placeholder1}/>
+            </div>
+            <h2 className='text-[#291507] font-semibold text-sm'>Cat 1</h2>
+          </div>
+          {/* Second grid item */}
+          <div className='flex flex-col gap-3'>
+            <div className='w-[135px] h-[135px]'>
+              <img className='w-full h-full rounded-xl' src={Placeholder2}/>
+            </div>
+            <h2 className='text-[#291507] font-semibold text-sm'>Cat 2</h2>
+          </div>
+          {/* Third grid item */}
+          <div className='flex flex-col gap-3'>
+            <div className='w-[135px] h-[135px]'>
+              <img className='w-full h-full rounded-xl' src={Placeholder3}/>
+            </div>
+            <h2 className='text-[#291507] font-semibold text-sm'>Cat 3</h2>
+          </div>
+          {/* Fourth grid item */}
+          <div className='flex flex-col gap-3'>
+            <div className='w-[135px] h-[135px]'>
+              <img className='w-full h-full rounded-xl' src={Placeholder4}/>
+            </div>
+            <h2 className='text-[#291507] font-semibold text-sm'>Cat 4</h2>
           </div>
         </div>
       </div>
